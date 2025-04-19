d38edefe362864744495b0d08d2700e25a945d02
HaoleHawaiian <alohaethan@yahoo.com>
2025-04-19 09:37:25 -0700

    Updated vit.py to work with data_loader, moved vit.py up a level

--- vit.py
+++ vit.py
@@ -10,9 +10,9 @@ from transformers import ViTForImageClassification, ViTImageProcessor
 # https://huggingface.co/docs/transformers/en/model_doc/vit
 # https://github.com/huggingface/transformers/blob/v4.51.3/src/transformers/models/vit/modeling_vit.py#L780
 # https://github.com/huggingface/transformers/blob/v4.51.3/src/transformers/models/vit/image_processing_vit.py#L42
-from torch.utils.data import DataLoader, random_split
+# from torch.utils.data import DataLoader, random_split
 from torch import nn, optim
-from torchvision import transforms
+# from torchvision import transforms
 
 # Viz, analysis
 import matplotlib.pyplot as plt
@@ -25,48 +25,32 @@ from tqdm import tqdm
 # https://tqdm.github.io/
 
 # data read class
-from data_reader import RealOrRenderedDataset
-
+# from data_reader import RealOrRenderedDataset
+from data_loader import get_train_test_loaders
 
 class ViTTrainer:
-    def __init__(self, csv_file, root_dir, save_path = "best_vit_model.pth", num_epochs = 5, batch_size = 64, val_split = 0.2, lr = 2e-5, weight_decay = 0.0):
+    def __init__(self, train_loader, test_loader, save_path='best_vit_model.pth', num_epochs=1, lr=2e-5, weight_decay=1e-2):
+        self.train_loader = train_loader
+        self.val_loader = test_loader  # test_loader is used for validation and evaluation
+        self.save_path = save_path
+        self.num_epochs = num_epochs
+        self.lr = lr
+        self.weight_decay = weight_decay
+
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         print(f"Using device: {self.device}")
 
-        self.save_path = save_path
-        self.num_epochs = num_epochs
-        self.batch_size = batch_size
+        # Load pretrained model and processor
+        self.model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224-in21k', num_labels=2).to(self.device)
 
-        # Call the pretrained models
-        self.model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224-in21k', num_labels = 2).to(self.device)
         self.processor = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224-in21k')
-        
-        # Resize/Preprocess imagees
-        transform = transforms.Compose([
-            transforms.Resize((224, 224)),
-            transforms.ToTensor(),
-            transforms.Normalize(mean = self.processor.image_mean, std = self.processor.image_std)
-        ])
-        # https://pytorch.org/vision/main/generated/torchvision.transforms.Compose.html
-
-        full_dataset = RealOrRenderedDataset(csv_file = csv_file, root_dir = root_dir, transform = transform)
-        val_size = int(val_split * len(full_dataset))
-        train_size = len(full_dataset) - val_size
-        
-        self.train_dataset, self.val_dataset = random_split(full_dataset, [train_size, val_size])
-        self.train_loader = DataLoader(self.train_dataset, batch_size = batch_size, shuffle = True)
-        self.val_loader = DataLoader(self.val_dataset, batch_size = batch_size, shuffle = False)
 
         self.criterion = nn.CrossEntropyLoss()
-        self.optimizer = optim.AdamW(self.model.parameters(), lr = lr, weight_decay = weight_decay)
-        # https://pytorch.org/docs/stable/generated/torch.optim.AdamW.html
-        
-        # Results at the end
+        self.optimizer = optim.AdamW(self.model.parameters(), lr=self.lr, weight_decay=self.weight_decay)
+
         self.train_losses = []
         self.val_losses = []
         self.val_accuracies = []
-        
-        # Save for initial best model, gets overwritten if the current model's accuracy is higher than the most recent
         self.best_val_acc = 0.0
 
     def train(self):
@@ -104,7 +88,7 @@ class ViTTrainer:
                 torch.save(self.model.state_dict(), self.save_path)
                 print(f"New best model saved with accuracy: {val_acc * 100:.2f}%")
 
-        self.plot_curves()
+        # self.plot_curves()
 
     # For evaluation at the end of the training -- NOT NEEDED FOR CALCULATIONS
     def validate(self):
@@ -192,15 +176,22 @@ class ViTTrainer:
             f.write(report)
 
 if __name__ == "__main__":
-    trainer = ViTTrainer(
-        csv_file ='data/train.csv',
-        root_dir ='data',
-        save_path ='best_vit_model.pth',
-        # Tuning here!
-        num_epochs = 2,
-        batch_size = 64,
-        lr = 2e-5,
-        weight_decay = 0.01        
+    train_loader, test_loader, _, _, _, _ = get_train_test_loaders(
+        csv_path='data/train.csv',
+        image_folder='data/train_data',
+        batch_size=128,
+        split_ratio=0.8,
+        augmentation=True
     )
+
+    trainer = ViTTrainer(
+        train_loader=train_loader,
+        test_loader=test_loader,
+        save_path='best_vit_model.pth',
+        num_epochs=1,
+        lr=2e-5,
+        weight_decay=0.01
+    )
+
     trainer.train()
     trainer.evaluate()
