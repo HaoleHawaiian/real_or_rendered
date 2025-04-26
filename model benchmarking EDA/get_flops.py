627a249fa0d24c54542c49546756668ea3627794
Anna Z <azhu95@gatech.edu>
2025-04-26 16:33:47 -0700

    add more models to flops

--- model benchmarking EDA/get_flops.py
+++ model benchmarking EDA/get_flops.py
@@ -1,9 +1,25 @@
 from ptflops import get_model_complexity_info
 from transformers import ViTForImageClassification
+import models.juanchitocnn
+import models.visiontransformer
+import models.efficientnetb0
 
 """
 Get the FLOPs of a model for reporting
 """
+# select_model = 'EfficientNet' # ['EfficientNet', 'VisionTransformer', 'JuanchitoCNN']
+
+# if select_model == 'EfficientNet':
+#     project_model = models.efficientnetb0.ProjectEfficientNet(epochs=epochs, learning_rate=learning_rate, batch_size=batch_size, optimizer=optimizer, momentum=momentum, weight_decay=weight_decay)
+#     print(f'Using model {select_model}')
+# elif select_model == 'VisionTransformer':
+#     project_model = models.visiontransformer.ProjectVisionTransformer(epochs=epochs, learning_rate=learning_rate, batch_size=batch_size, optimizer=optimizer, momentum=momentum, weight_decay=weight_decay)
+#     print(f'Using model {select_model}')
+# elif select_model == 'JuanchitoCNN':
+#     project_model = models.juanchitocnn.ProjectJuanchitoCNN(epochs=epochs, learning_rate=learning_rate, batch_size=batch_size, optimizer=optimizer, momentum=momentum, weight_decay=weight_decay)
+#     print(f'Using model {select_model}')
+# else:
+#     print('No valid model selected')
 
 # model = ViTForImageClassification.from_pretrained('facebook/deit-tiny-distilled-patch16-224', num_labels=self.num_classes).to(self.device)
 model = ViTForImageClassification.from_pretrained('facebook/deit-tiny-distilled-patch16-224', num_labels=2)
@@ -17,4 +33,4 @@ macs, params = get_model_complexity_info(
 )
 
 flops = macs * 2  # Convert MACs to FLOPs
-print(f"FLOPs: {flops/1e9:.2f} G")  # Expected ~1.3 GFLOPs
+print(f"FLOPs: {flops/1e9:.2f} G") # 1 gigaflop = 1 billion flop
