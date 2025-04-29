2ebe2f900d7101bd21739db1cf035eb4b99be6a0
jrdelag2 <jrdelag2@yahoo.com>
2025-04-28 20:37:10 -0500

    Adversarial image generation
    Pushed code to generate images from adversarial attacks

--- FGSM_attack.py
+++ FGSM_attack.py
@@ -141,7 +141,7 @@ class FGSMAttacker:
     def _fgsm_attack(self, image, epsilon, data_grad):
         sign_data_grad = data_grad.sign()
         perturbed_image = image + epsilon * sign_data_grad
-        return torch.clamp(perturbed_image, 0, 1)
+        return torch.clamp(perturbed_image, 0, 1), epsilon * sign_data_grad
     
     def _show_image_with_confidence(self, image, pred, pred_confidence, save_path=None):
         # Convert tensor to numpy for plotting
@@ -162,7 +162,10 @@ class FGSMAttacker:
     
     def attack(self, test_loader, epsilon):
         correct = 0
+        original_images=[]
         adv_examples = []
+        perturbation_list = []
+        perturbation_list_2 = []
         shown_images = 0 
     
         for i, (data, target) in enumerate(test_loader):
@@ -192,7 +195,7 @@ class FGSMAttacker:
     
                 data_grad = image.grad.data
                 image_denorm = self._denormalize(image)
-                perturbed_image = self._fgsm_attack(image_denorm, epsilon, data_grad)
+                perturbed_image, perturbation = self._fgsm_attack(image_denorm, epsilon, data_grad)
                 perturbed_image_norm = self.normalize(perturbed_image)
     
     
@@ -203,7 +206,11 @@ class FGSMAttacker:
                     final_pred = output.max(1, keepdim=True)[1]
                 
                 # Calculate confidence for output image
-                softmax_output = F.softmax(output.logits, dim=1)
+                try:
+                    softmax_output = F.softmax(output.logits, dim=1)
+                except:
+                    softmax_output = F.softmax(output, dim=1)
+                
                 confidence = softmax_output[0][final_pred.item()] * 100 
                 
                 if final_pred.item() == label.item():
@@ -218,8 +225,14 @@ class FGSMAttacker:
                         shown_images += 1
                 else:
                     if len(adv_examples) < 5:
+                        original_images.append((init_pred.item(), final_pred.item(),
+                                             image_denorm.squeeze().detach().cpu().numpy()))
                         adv_examples.append((init_pred.item(), final_pred.item(),
                                              perturbed_image.squeeze().detach().cpu().numpy()))
+                        perturbation_list.append((init_pred.item(), final_pred.item(),
+                                             perturbation.squeeze().detach().cpu().numpy()))
+                        perturbation_list_2.append((init_pred.item(), final_pred.item(),
+                                             (perturbed_image-image_denorm).squeeze().detach().cpu().numpy()))
                     if shown_images < 3 and confidence < 15:   #
                         save_path = f"adv_examples/epsilon_{epsilon}_img_{shown_images+1}.png"
                         self._show_image_with_confidence(perturbed_image, final_pred.item(), confidence, save_path=save_path)
@@ -230,4 +243,4 @@ class FGSMAttacker:
     
         final_acc = correct / float(len(test_loader.dataset))
         print(f"Epsilon: {epsilon}\tTest Accuracy = {correct} / {len(test_loader.dataset)} = {final_acc}")
-        return final_acc, adv_examples
+        return final_acc, original_images, adv_examples, perturbation_list, perturbation_list_2
