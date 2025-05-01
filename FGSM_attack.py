8926572e13d0ef128448f178a473b9f0479b9a35
HaoleHawaiian <alohaethan@yahoo.com>
2025-04-30 17:49:20 -0700

    Removed image constraints like 'only show 3 images under 40% confidence'

--- FGSM_attack.py
+++ FGSM_attack.py
@@ -106,24 +106,24 @@ class FGSMAttacker:
                         adv_examples.append((init_pred.item(), final_pred.item(),
                                              perturbed_image.squeeze().detach().cpu().numpy()))
 
-                if confidence < confidence_threshold and shown_images < 5:
-                    save_path = f"adv_examples/epsilon_{epsilon}_img_{shown_images+1}.png"
-                    self._show_image_with_confidence(perturbed_image, final_pred.item(), confidence, save_path=save_path)
-                    shown_images += 1
+                # if confidence < confidence_threshold and shown_images < 5:
+                #     save_path = f"adv_examples/epsilon_{epsilon}_img_{shown_images+1}.png"
+                #     self._show_image_with_confidence(perturbed_image, final_pred.item(), confidence, save_path=save_path)
+                #     shown_images += 1
                     
-                if shown_images < 3 and confidence < 40:
-                    save_path = f"adv_examples/epsilon_{epsilon}_img_{shown_images+1}.png"
-                    self._show_image_with_confidence(perturbed_image, final_pred.item(), confidence, save_path=save_path)
-                    shown_images += 1
+                # if shown_images < 3 and confidence < 40:
+                #     save_path = f"adv_examples/epsilon_{epsilon}_img_{shown_images+1}.png"
+                #     self._show_image_with_confidence(perturbed_image, final_pred.item(), confidence, save_path=save_path)
+                #     shown_images += 1
 
                     perturbation_list.append((init_pred.item(), final_pred.item(),
                                              perturbation.squeeze().detach().cpu().numpy()))
                     perturbation_list_2.append((init_pred.item(), final_pred.item(),
                                              (perturbed_image-image_denorm).squeeze().detach().cpu().numpy()))
-                    if shown_images < 3 and confidence < 15:   #
-                        save_path = f"adv_examples/epsilon_{epsilon}_img_{shown_images+1}.png"
-                        self._show_image_with_confidence(perturbed_image, final_pred.item(), confidence, save_path=save_path)
-                        shown_images += 1
+                    # if shown_images < 3 and confidence < 15:   #
+                    #     save_path = f"adv_examples/epsilon_{epsilon}_img_{shown_images+1}.png"
+                    #     self._show_image_with_confidence(perturbed_image, final_pred.item(), confidence, save_path=save_path)
+                    #     shown_images += 1
 
     
             if i % 2000 == 1999:
